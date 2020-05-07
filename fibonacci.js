8139ca771c9e098ed194b01a713484f3a54301e0
Pavlov Oleh <opavlov@conceptsoft.io>
2020-05-08 07:28:27 +0900

    converted to bigint

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+const calcFibonacci = n => { 
+    let i = 0;
+    const sequence = [0n, 1n];
+    while (!sequence[n]) {
+        sequence.push(BigInt(sequence[i] + sequence[++i]))
+    } 
+    return sequence[n]
+}
+
+[1, 2, 3, 4, 5, 6, 250, 1000, 10000].forEach((n, i) => {
+    console.time('PERFOMANCE ');
+    console.log('TEST CASE %d; FIBONACCI N = %d; RESULT = %d.', i + 1, n, calcFibonacci(n));
+    console.timeEnd('PERFOMANCE ');
+})
